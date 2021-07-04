7a3c994169d4877e62d53f85e40714b7d4046572
Connie-exe <constanzafiorella.duartepetti@gmail.com>
2021-07-04 04:11:31 -0300

    a medio camino de linea de atención

--- Integrador/Assets/Scripts/Intantiator_Controller.cs
+++ Integrador/Assets/Scripts/Intantiator_Controller.cs
@@ -7,6 +7,7 @@ public class Intantiator_Controller : MonoBehaviour
     public List<GameObject> patients;
     public GameObject instantiatePos;
     public float timer = 7;
+    private int cont = 0;
     //private float time = 0;
     void Start()
     {
@@ -22,12 +23,13 @@ public class Intantiator_Controller : MonoBehaviour
 
     private void SpawnPatients()
     {
-        if (timer <= 0)//si timer es menor o igual a 0
+        if (timer <= 0 && cont <= 4)//si timer es menor o igual a 0
         {
             float offsetX = instantiatePos.transform.position.x;//se declara un float offsetX que es igual a la posición de la posición del instantiatePos en el eje x en unity
             int rnd = UnityEngine.Random.Range(0, patients.Count);//se declara un int (rnd) que genera un número random entre 0 y el número máximo de pacientes que hay en la lista patients
             Vector3 transform = new Vector3(offsetX, instantiatePos.transform.position.y, instantiatePos.transform.position.z);//aquí se declara la nueva posición del instantiate en el entorno 3d de unity
             Instantiate(patients[rnd], transform, Quaternion.identity);//esto permite que los enemigos spwneen en random lugares sin repetición
+            cont++;
             timer = 7;//una vez que termine el for el timer valdrá 7 y saldrá del if
         }
     }
--- Integrador/Assets/Scripts/Patient_Controller.cs
+++ Integrador/Assets/Scripts/Patient_Controller.cs
@@ -1,18 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Patient_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //public GameObject reception;
+    //public GameObject testation;
+    //public GameObject vaccination;
+    public List<GameObject> stations;
+    private NavMeshAgent patient;
+    public  bool reception_occupied;
+    public  bool testation_occupied;
+    public  bool vaccination_occupied;
+    public float timer;
+    public int cont_station = 0;
     void Start()
     {
-        
+        reception_occupied = false;
+        testation_occupied = false;
+        vaccination_occupied = false;
+        timer = 5f;
+        patient = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        NextStation();
     }
+
+    private void NextStation()
+    {
+        if(reception_occupied == false && cont_station == 0)
+        {
+            patient.SetDestination(stations[cont_station].transform.position);
+            if(reception_occupied == true)
+            {
+                timer -= Time.deltaTime;
+            }
+            if (timer <= 0)
+            {
+                timer = 5;
+                cont_station++;
+            }
+        }
+        if(testation_occupied == false && cont_station == 1)
+        {
+            patient.SetDestination(stations[cont_station].transform.position);
+            reception_occupied = false;
+        }
+        if (vaccination_occupied == false && cont_station == 2)
+        {
+            patient.SetDestination(stations[cont_station].transform.position);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)//si collisiona con...
+    {
+        if (collision.gameObject.CompareTag("Reception"))
+        {
+            reception_occupied = true;
+            Debug.Log("Paciente en Recepción");
+        }
+
+        if (collision.gameObject.CompareTag("Testation"))
+        {
+            testation_occupied = true;
+            //timer -= Time.deltaTime;
+            //if (timer <= 0)
+            //{
+            //    testation_occupied = false;
+            //    timer = 5;
+            //    cont_station++;
+            //}
+            Debug.Log("Paciente en Testation");
+        }
+
+        if (collision.gameObject.CompareTag("Vaccination"))
+        {
+            vaccination_occupied = true;
+            //timer -= Time.deltaTime;
+            //if (timer <= 0)
+            //{
+            //    vaccination_occupied = false;
+            //    timer = 5;
+            //    cont_station++;
+            //}
+            Debug.Log("Paciente en Vaccination");
+        }
+    }
+
 }
