7f1eec9e966006db38623e2f0938d7f8ca98e4fc
Connie-exe <constanzafiorella.duartepetti@gmail.com>
2021-11-11 00:46:45 -0300

    seteo_scene_minijuego_sintomas

---
+++ Integrador/Assets/Scripts/Diagnostico/MediciónSintomas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediciónSintomas : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z + transform.position.z);
+        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objectPosition;
+        rb.isKinematic = true;
+    }
+}
