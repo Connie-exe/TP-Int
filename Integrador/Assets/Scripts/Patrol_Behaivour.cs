44ea2f86e6f66accec385758008f2506b593ad13
Connie-exe <constanzafiorella.duartepetti@gmail.com>
2021-11-12 03:14:56 -0300

    several adds and changes

---
+++ Integrador/Assets/Scripts/Patrol_Behaivour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol_Behaivour : MonoBehaviour
+{
+    internal NavMeshAgent agent;
+    internal Vector3 destination;
+    public float destinationTime = 4;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = new Vector3(UnityEngine.Random.Range(-10, 12), 1, UnityEngine.Random.Range(-12, 9));
+    }
+
+
+    void Update()
+    {
+        PatrolBehaviour();
+    }
+
+    private void PatrolBehaviour()
+    {
+        agent.SetDestination(destination);//se pone una destinación mediante el navmesh
+        destinationTime -= Time.deltaTime;//el tiempo de destinación disminuye a medida que pasa el tiempo en unity
+        if (destinationTime < 0)//si el tiempo de destinación es menor a 0
+        {
+            destination = new Vector3(Random.Range(-10, 12), 1, Random.Range(-12, 9));//la destinación se define con 2 valores random en el espacio, en sus ejes x y z
+            destinationTime = 4;//y el valor del tiempo de destinación pasa a valer 4
+        }
+    }
+}
---
+++ Integrador/Assets/Scripts/Protesta_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protesta_Controller : MonoBehaviour
+{
+    public GameObject instantiator;
+    void Start()
+    {
+        instantiator.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
