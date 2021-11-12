cd2ff5dbd5ffc6185f3852364f18e8912dd02e40
Connie-exe <constanzafiorella.duartepetti@gmail.com>
2021-11-12 01:53:08 -0300

    starting minigame vaccination child

---
+++ Integrador/Assets/Scripts/Vaccine_MiniGame/EntertainingChild.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntertainingChild : MonoBehaviour
+{
+    public GameObject im_confused;
+    public Text txt_confused;
+    public static bool b_confused;
+    private int randomnum;
+    void Start()
+    {
+        im_confused.SetActive(false);
+        txt_confused.text = "";
+        b_confused = false;
+    }
+    void Update()
+    {
+        //Distraction();
+    }
+
+    public void Distraction()
+    {
+        Debug.Log("entro distraction");
+        randomnum = Random.Range(1, 3);
+        switch (randomnum)
+        {
+            case 1:
+                im_confused.SetActive(true);
+                b_confused = true;
+                txt_confused.text = "Excelente, el infante está distraído, ahora no llorará por la vacuna!";
+                break;
+            default:
+                txt_confused.text = "Esa distracción no ha surtido efecto, prueba con otra.";
+                break;
+        }
+    }
+}
---
+++ Integrador/Assets/Scripts/Vaccine_MiniGame/VacApplication_Children.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VacApplication_Children : MonoBehaviour
+{
+    public Text txt_supplies;
+    public Image fillimage;
+    public float timer;
+    private float waitTime;
+    public Text txt_cryingbaby;
+    void Start()
+    {
+        txt_supplies.text = "Supplies: " + MoneySystem.cant_vac;
+        waitTime = timer;
+        //fillimage = GetComponent<Image>();
+    }
+    void Update()
+    {
+        SetTimer();
+    }
+
+    public void SetTimer()
+    {
+        waitTime -= Time.deltaTime;
+        fillimage.fillAmount = waitTime / timer;
+
+        if (waitTime <= 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Vaccine") && EntertainingChild.b_confused == true)
+        {           
+            MoneySystem.cant_vac--;
+            txt_supplies.text = "Supplies: " + MoneySystem.cant_vac;
+            Destroy(other.gameObject);
+            StartCoroutine(BackToGame());           
+        }
+        if (other.gameObject.CompareTag("Vaccine") && EntertainingChild.b_confused == false)
+        {
+            MoneySystem.cant_vac--;
+            txt_supplies.text = "Supplies: " + MoneySystem.cant_vac;
+            txt_cryingbaby.text = "Oh no! El infante no permitió que le pusieras la vacuna porque no lo has distraido de forma correcta.";
+            Destroy(other.gameObject);
+            StartCoroutine(BackToGame());
+        }
+    }
+
+    IEnumerator BackToGame()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
