c4f76e75368c923a24ad491f4fee106ec5a05d5e
Connie-exe <constanzafiorella.duartepetti@gmail.com>
2021-12-22 02:42:49 -0300

    links and credits

---
+++ Integrador/Assets/Scripts/Links.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Links : MonoBehaviour
+{
+    public string str_link;    
+    void Update()
+    {
+        ButtonLink(str_link);
+    }
+
+    public void ButtonLink(string enlace)
+    {
+        Application.OpenURL(enlace);
+    }
+}
